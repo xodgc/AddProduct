588038ce54cce5069af9f992ff92aed7a7c25ad5
David Cook <deathkick4@gmail.com>
2021-05-06 15:13:52 -0700

    added ts

---
+++ ts/main.ts
@@ -0,0 +1,9 @@
+window.onload = function() {
+    let addBtn = 
+        <HTMLElement>document.querySelector("input[type=button]");
+    addBtn.onclick = addVideoGame;
+}
+
+funtion addVideoGame() {
+    alert("test");
+}
