a7e6e8c113c30abaa88d3b444310de322f6089b9
David Cook <deathkick4@gmail.com>
2021-05-06 16:19:47 -0700

    Almost finished

--- js/main.js
+++ js/main.js
@@ -35,6 +35,20 @@ function getVideoGame() {
     return game;
 }
 function displayGame(myGame) {
+    var displayDiv = getById("display");
+    var gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
+    var gameInfo = document.createElement("p");
+    var gameMediumDisplay = "";
+    if (myGame.isDigitalOnly) {
+        gameMediumDisplay = "This is a digital only game.";
+    }
+    else {
+        gameMediumDisplay = "You can come and buy a physical copy!";
+    }
+    gameInfo.innerText = myGame.title + " has a rating of " + myGame.rating + ". It costs " + myGame.price + ". " + gameMediumDisplay;
+    displayDiv.appendChild(gameHeading);
+    displayDiv.appendChild(gameInfo);
 }
 function isAllDataValid() {
     return true;
--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,8 @@
 class VideoGame {
-    title:string;
-    price:number;
-    rating:string;
-    isDigitalOnly:boolean;
+  title: string;
+  price: number;
+  rating: string;
+  isDigitalOnly: boolean;
 }
 
 //test code
@@ -13,23 +13,22 @@ myGame.rating = "E";
 myGame.isDigitalOnly = true;
 */
 
-window.onload = function() {
-    let addBtn = 
-        <HTMLElement>document.querySelector("input[type=button]");
-    addBtn.onclick = addVideoGame;
-}
+window.onload = function () {
+  let addBtn = <HTMLElement>document.querySelector("input[type=button]");
+  addBtn.onclick = addVideoGame;
+};
 
 function addVideoGame() {
-    console.log("Add video game was called");
+  console.log("Add video game was called");
 
-    if(isAllDataValid()) {
-        let game = getVideoGame();
-        displayGame(game);
-    }
+  if (isAllDataValid()) {
+    let game = getVideoGame();
+    displayGame(game);
+  }
 }
 
-function getById(id:string) {
-    return document.getElementById(id);
+function getById(id: string) {
+  return document.getElementById(id);
 }
 
 /**
@@ -37,35 +36,53 @@ function getById(id:string) {
  * and returns it in a VideoGame object
  */
 
-function getVideoGame():VideoGame {
-    let game = new VideoGame();
-    // TODO: populate with data from the form
-    let inputTitle = <HTMLInputElement>getById("title");
-    game.title = inputTitle.value;
+function getVideoGame(): VideoGame {
+  let game = new VideoGame();
+  // TODO: populate with data from the form
+  let inputTitle = <HTMLInputElement>getById("title");
+  game.title = inputTitle.value;
 
-    let priceInput = <HTMLInputElement>getById("price");
-    game.price = parseFloat(priceInput.value);
+  let priceInput = <HTMLInputElement>getById("price");
+  game.price = parseFloat(priceInput.value);
 
-    let ratingInput = <HTMLSelectElement>getById("rating");
-    game.rating = ratingInput.value;
+  let ratingInput = <HTMLSelectElement>getById("rating");
+  game.rating = ratingInput.value;
 
-    let digitalOnly = <HTMLInputElement>getById("online");
-    if(digitalOnly.checked) {
-        game.isDigitalOnly = true;
-    }
-    else {
-        game.isDigitalOnly = false;
-    }
-    return game;
+  let digitalOnly = <HTMLInputElement>getById("online");
+  if (digitalOnly.checked) {
+    game.isDigitalOnly = true;
+  } 
+  else {
+    game.isDigitalOnly = false;
+  }
+  return game;
 
-    // TODO: Return game
+  // TODO: Return game
 }
 
-function displayGame(myGame:VideoGame):void {
-    //TODO: display video game below form
+function displayGame(myGame: VideoGame): void {
+  let displayDiv = getById("display");
+
+  let gameHeading = document.createElement("h2");
+  gameHeading.innerText = myGame.title;
+
+  let gameInfo = document.createElement("p");
+  let gameMediumDisplay = "";
+  if (myGame.isDigitalOnly) {
+    gameMediumDisplay = "This is a digital only game.";
+  } 
+  else {
+    gameMediumDisplay = "You can come and buy a physical copy!";
+  }
+
+  gameInfo.innerText = `${myGame.title} has a rating of ${myGame.rating}. It costs ${myGame.price}. ${gameMediumDisplay}`;
+
+  displayDiv.appendChild(gameHeading);
+
+  displayDiv.appendChild(gameInfo);
 }
 
 // Add validation code**************************************************
 function isAllDataValid() {
-    return true;
+  return true;
 }
