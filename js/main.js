82fab8df36f413c4a1c212d059575ed44bfea726
David Cook <deathkick4@gmail.com>
2021-05-06 15:46:14 -0700

    Added more functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
+var VideoGame = (function () {
+    function VideoGame() {
+    }
+    return VideoGame;
+}());
 window.onload = function () {
     var addBtn = document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
-    alert("test");
+    console.log("Add video game was called");
+    if (isAllDataValid()) {
+        var game = getVideoGame();
+        displayGame(game);
+    }
+}
+function getById(id) {
+    return document.getElementById(id);
+}
+function getVideoGame() {
+    var game = new VideoGame();
+    var inputTitle = getById("title");
+    game.title = inputTitle.value;
+    var priceInput = getById("price");
+    game.price = parseFloat(priceInput.value);
+    var rating = ;
+}
+function displayGame(myGame) {
+}
+function isAllDataValid() {
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,18 @@
+class VideoGame {
+    title:string;
+    price:number;
+    rating:string;
+    isDigitalOnly:boolean;
+}
+
+//test code
+/*
+let myGame = new VideoGame();
+myGame.title = "Mario";
+myGame.rating = "E";
+myGame.isDigitalOnly = true;
+*/
+
 window.onload = function() {
     let addBtn = 
         <HTMLElement>document.querySelector("input[type=button]");
@@ -5,5 +20,37 @@ window.onload = function() {
 }
 
 function addVideoGame() {
-    alert("test");
+    console.log("Add video game was called");
+
+    if(isAllDataValid()) {
+        let game = getVideoGame();
+        displayGame(game);
+    }
+}
+
+function getById(id:string) {
+    return document.getElementById(id);
+}
+
+function getVideoGame():VideoGame {
+    let game = new VideoGame();
+    // TODO: populate with data from the form
+    let inputTitle = <HTMLInputElement>getById("title");
+    game.title = inputTitle.value;
+
+    let priceInput = <HTMLInputElement>getById("price");
+    game.price = parseFloat(priceInput.value);
+
+    let rating = 
+
+    // TODO: Return game
+}
+
+function displayGame(myGame:VideoGame):void {
+    //TODO: display video game below form
+}
+
+// Add validation code**************************************************
+function isAllDataValid() {
+    return true;
 }
